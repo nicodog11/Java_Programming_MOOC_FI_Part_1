dd6f3057dadac359c1bef5bd2f71918e9a11941c
nicodog11 <59406836+nicodog11@users.noreply.github.com>
2024-06-27 08:45:48 -0500

    Add files via upload

---
+++ part04-Part04_05.Whistle/src/main/java/Whistle.java
@@ -0,0 +1,11 @@
+public class Whistle {
+    private String sound;
+
+    public Whistle(String whistleSounds){
+        this.sound = whistleSounds;
+    }
+
+    public void sound(){
+        System.out.println(sound);
+    }
+}
